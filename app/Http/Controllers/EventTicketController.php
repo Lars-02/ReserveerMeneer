9f920de54fc9ecd02070643f80b5560da5d7bb31
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-10 15:29:26 +0200

    Add CSV and JSON Download

--- app/Http/Controllers/EventTicketController.php
+++ app/Http/Controllers/EventTicketController.php
@@ -11,6 +11,9 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class EventTicketController extends Controller
 {
@@ -70,7 +73,7 @@ class EventTicketController extends Controller
      */
     public function show(EventTicket $eventTicket)
     {
-        return 'success';
+
     }
 
     /**
@@ -105,5 +108,45 @@ class EventTicketController extends Controller
     public function destroy(EventTicket $eventTicket)
     {
         //
+    }
+
+    public function downloadJSON(): BinaryFileResponse
+    {
+        $tickets = EventTicket::where('user_id', Auth::user()->id)->get(['id', 'event_id', 'user_id', 'start_at', 'end_at', 'firstname', 'lastname', 'birthday']);
+        $filename = "tickets.json";
+        $handle = fopen($filename, 'w+');
+        fputs($handle, $tickets->toJson(JSON_PRETTY_PRINT));
+        fclose($handle);
+        $headers = array('Content-type'=> 'application/json');
+        return response()->download($filename,'tickets.json',$headers);
+    }
+
+    /**
+     * @return StreamedResponse
+     */
+    public function downloadCSV(): StreamedResponse
+    {
+        $headers = array(
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=tickets.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0"
+        );
+
+        $tickets = EventTicket::where('user_id', Auth::user()->id)->get();
+        $columns = array('Ticket ID', 'Event ID', 'User ID', 'Start at', 'Ends at', 'Firstname', 'Lastname', 'Birthday');
+
+        $callback = function() use ($tickets, $columns)
+        {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach($tickets as $ticket) {
+                fputcsv($file, array($ticket->id, $ticket->event_id, $ticket->user_id, $ticket->start_at, $ticket->end_at, $ticket->firstname, $ticket->lastname, $ticket->birthday));
+            }
+            fclose($file);
+        };
+        return Response::stream($callback, 200, $headers);
     }
 }

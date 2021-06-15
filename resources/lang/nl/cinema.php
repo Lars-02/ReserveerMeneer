237c983751eaa5d7f3e7c831d60f187aed7e4cc4
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-15 23:00:44 +0200

    Add Cinema Dutch Localization

--- resources/lang/nl/cinema.php
+++ resources/lang/nl/cinema.php
@@ -1,7 +1,23 @@
 <?php
 
 return [
-
-    'title' => 'Bekijk onze bioscopen',
-
+    'title' => 'Bioscopen',
+    'my.tickets' => 'Mijn films',
+    'my.hall' => 'Jouw zaal is :hall',
+    'cinema' => 'Bioscoop',
+    'hall' => 'Bioscoop zaal :index',
+    'halls' => 'Bioscoop zalen',
+    'halls.number' => 'Er zij :halls zalen in deze bioscoop',
+    'halls.number.movie' => 'Er zijn :halls zalen voor deze film',
+    'rows' => 'Er zijn :rows rijen in deze zaal',
+    'seats' => 'Er zijn :seats stoelen in deze zaal',
+    'movie' => 'Film',
+    'movies' => 'Films',
+    'movies.number' => 'Er zijn :movies films in deze zaal',
+    'movies.cinema' => 'Alle films in deze bioscoop',
+    'duration' => 'De film duurt :duration',
+    'minimum_age' => 'Je moet minstens :age jaar oud zijn',
+    'time' => 'Tijd',
+    'starting_at' => 'Start op :date',
+    'seat' => 'Jouw stoel',
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,20 +19,23 @@
         </div>
         <div class="flex-grow grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
             @foreach($items as $item)
-                <div class="h-full bg-white rounded shadow p-4 flex flex-col content-between ">
+                <div class="h-full bg-white rounded shadow p-4 flex flex-col">
                     @if(get_class($item) == \App\Models\Event::class)
-                        <div class="bg-gray-500 w-full text-xl text-white text-center font-bold rounded-lg">Event</div>
-                        <h2>{{ $item->name }}</h2>
-                        <h3><i class="fas fa-calendar-day"></i> {{ __('event.date') }}</h3>
-                        <p>{{ __('event.dates', ['startDate' => $item->start, 'endDate' => $item->end]) }}</p>
-                        <h3><i class="fas fa-city"></i> {{ __('general.location') }}</h3>
-                        <p>{{ __('general.location.city', ['city' => $item->city]) }}</p>
-                        <p>{{ $item->house_number . ' ' . $item->streetname}}</p>
-                        <p>{{ $item->city . ', ' . $item->country_code }}</p>
-                        <h3><i class="fas fa-city"></i> {{ __('event.tickets') }}</h3>
-                        <p>{{ __('event.tickets.remaining', ['remaining' => $item->remainingTickets, 'total' => $item->total_tickets]) }}</p>
-                        <p>{{ __('event.tickets.userMax', ['max' => $item->max_user_tickets]) }}</p>
-                        <div class="flex justify-between mt-4">
+                        <div class="flex flex-col">
+                            <div
+                                class="bg-gray-500 w-full text-xl text-white text-center font-bold rounded-lg">{{ __('event.title') }}</div>
+                            <h2>{{ $item->name }}</h2>
+                            <h3><i class="fas fa-calendar-day"></i> {{ __('event.date') }}</h3>
+                            <p>{{ __('event.dates', ['startDate' => $item->start, 'endDate' => $item->end]) }}</p>
+                            <h3><i class="fas fa-city"></i> {{ __('general.location') }}</h3>
+                            <p>{{ __('general.location.city', ['city' => $item->city]) }}</p>
+                            <p>{{ $item->house_number . ' ' . $item->streetname}}</p>
+                            <p>{{ $item->city . ', ' . $item->country_code }}</p>
+                            <h3><i class="fas fa-city"></i> {{ __('event.tickets') }}</h3>
+                            <p>{{ __('event.tickets.remaining', ['remaining' => $item->remainingTickets, 'total' => $item->total_tickets]) }}</p>
+                            <p>{{ __('event.tickets.userMax', ['max' => $item->max_user_tickets]) }}</p>
+                        </div>
+                        <div class="flex flex-row flex-grow items-end justify-between mt-4">
                             @can('update', $item)
                                 <a href="{{ route('event.edit', $item) }}">
                                     <x-button>{{ __('general.edit') }}</x-button>
@@ -45,24 +48,30 @@
                             @endcan
                         </div>
                     @else
-                        <div class="bg-gray-500 w-full text-xl text-white text-center font-bold rounded-lg">Movie</div>
-                        <h2>{{ $item->movie->name }}</h2>
-                        <h3><i class="fas fa-city"></i> {{ $item->cinemaHall->cinema->name }}</h3>
-                        <p>{{ __('general.location.city', ['city' => $item->cinemaHall->cinema->city]) }}</p>
-                        <p>{{ $item->cinemaHall->cinema->house_number . ' ' . $item->cinemaHall->cinema->streetname}}</p>
-                        <p>{{ $item->cinemaHall->cinema->city . ', ' . $item->cinemaHall->cinema->country_code }}</p>
-                        <h3><i class="fas fa-calendar-day"></i> {{ __('cinema.time') }}</h3>
-                        <p>{{ __('cinema.starting_at', ['date' => $item->start]) }}</p>
-                        <h3><i class="fas fa-video"></i> {{ __('cinema.movie') }}</h3>
-                        <p>{{ __('cinema.duration', ['duration' => $item->movie->duration]) }}</p>
-                        <p>{{ __('cinema.minimum_age', ['age' => $item->movie->minimum_age]) }}</p>
-                        @can('create', \App\Models\MovieTicket::class)
-                            <div class="flex items-center mt-4">
-                                <a href="{{ route('movie.buy', $item) }}">
-                                    <x-button>{{ __('general.buy') }}</x-button>
-                                </a>
-                            </div>
-                        @endcan
+                        <div class="flex flex-col">
+                            <div
+                                class="bg-gray-500 w-full text-xl text-white text-center font-bold rounded-lg">{{ __('cinema.title') }}</div>
+                            <h2>{{ $item->movie->name }}</h2>
+                            <h3><i class="fas fa-city"></i> {{ $item->cinemaHall->cinema->name }}</h3>
+                            <p>{{ __('general.location.city', ['city' => $item->cinemaHall->cinema->city]) }}</p>
+                            <p>{{ $item->cinemaHall->cinema->house_number . ' ' . $item->cinemaHall->cinema->streetname}}</p>
+                            <p>{{ $item->cinemaHall->cinema->city . ', ' . $item->cinemaHall->cinema->country_code }}</p>
+                            <h3><i class="fas fa-calendar-day"></i> {{ __('cinema.time') }}</h3>
+                            <p>{{ __('cinema.starting_at', ['date' => $item->start]) }}</p>
+                            <h3><i class="fas fa-video"></i> {{ __('cinema.movie') }}</h3>
+                            <p>{{ __('cinema.duration', ['duration' => $item->movie->duration]) }}</p>
+                            <p>{{ __('cinema.minimum_age', ['age' => $item->movie->minimum_age]) }}</p>
+                        </div>
+                        <div class="flex flex-row flex-grow items-end justify-between mt-4">
+
+                            @can('create', \App\Models\MovieTicket::class)
+                                <div class="flex items-center mt-4">
+                                    <a href="{{ route('movie.buy', $item) }}">
+                                        <x-button>{{ __('general.buy') }}</x-button>
+                                    </a>
+                                </div>
+                            @endcan
+                        </div>
                     @endif
                 </div>
             @endforeach

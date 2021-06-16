e39df556047848f6cefe42db163b17153a4245d2
Maik de Kruif <maik@maik.dev>
2021-06-16 10:44:59 +0200

    Test isTimeSlot rule

--- tests/Unit/RulesTest.php
+++ tests/Unit/RulesTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Rules\IsAfterNow;
+use App\Rules\IsTimeSlot;
 use Illuminate\Support\Facades\Date;
 use PHPUnit\Framework\TestCase;
 
@@ -19,5 +20,24 @@ class RulesTest extends TestCase
         $this->assertTrue(
             $isAfterNow->passes(null, date("Y-m-d H:i:s", strtotime(Date::now()) + 60))
         );
+    }
+
+    public function testIsTimeSlot()
+    {
+        $isTimeSlot = new IsTimeSlot();
+
+        for ($i = 0; $i < 60; $i += 1) {
+            $time = Date::now();
+            $time->setTime(0, $i, 0);
+            if ($i % 30 === 0) {
+                $this->assertTrue(
+                    $isTimeSlot->passes(null, $time)
+                );
+            } else {
+                $this->assertFalse(
+                    $isTimeSlot->passes(null, $time)
+                );
+            }
+        }
     }
 }
